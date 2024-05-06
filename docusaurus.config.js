2430eed514d127e6ff5f52fe80b9edebc11bf8f0
Athi <athi@botlhale.ai>
2024-05-06 15:31:50 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const config = {
   title: 'Vela',
   tagline: "Documentation",
   url: 'https://botlhale-ai.github.io/vela-docs/', 
-  baseUrl: '/vela-docs/',
+  baseUrl: '',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: '/img/favicon.ico',
