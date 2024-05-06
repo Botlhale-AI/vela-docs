129827191a2fa855cc8f2aaaf8c0939868cc1c50
Athi <athi@botlhale.ai>
2024-05-06 16:16:41 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: '/img/favicon.ico',
   organizationName: 'botlhale-AI', 
-  projectName: 'vela-docs', 
+  projectName: 'botlhale-ai.github.io/vela-docs', 
   deploymentBranch: 'main', 
   trailingSlash: false,
 
